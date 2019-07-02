6c48dfa00c9856a8d5dd93351bf4fee5622f0ce3
Emre Can ÖZTAŞ <me@emrecanoztas.com>
2019-07-03 01:07:50 +0300

    Güncelleme

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,19 @@
 // Package(s).
-var gulp    = require('gulp');
-var jade    = require('gulp-jade');
-
-// Setting(s).
-var dev = {
-	'jade': [
-		{
-			'src': 'jade-kit/*.jade',
-			'dist': './',
-			'task-name': 'jade'
-		}
-	]
-};
+var gulp = require('gulp');
+var jade = require('gulp-jade');
 
 // TASK 0: Default
-gulp.task('default', function() {
-    console.log('Works fine!');
+gulp.task('default', function () {
+	console.log('Works fine!');
 });
 
 // TASK 1: Jade
-gulp.task(dev.jade[0]['task-name'], function() {
-	return(
-		gulp.src(dev.jade[0]['src'])
-		.pipe(jade({
-			pretty: true
-		}))
-		.pipe(gulp.dest(dev.jade[0]['dist']))
+gulp.task('jade', function () {
+	return (
+		gulp.src('./*.jade')
+			.pipe(jade({
+				pretty: true
+			}))
+			.pipe(gulp.dest('./'))
 	);
 });
