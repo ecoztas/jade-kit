5e7c66d44b65c42e4b1eaa482b463b983ff2a7bf
Emre Can ÖZTAŞ <me@emrecanoztas.com>
2019-09-23 13:19:42 +0300

    Güncelleme

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+/**
+ * -----------------------------------------------------------------------------
+ * gulpfile.js
+ * Author: Emre Can ÖZTAŞ <me@emrecanoztas.com>
+ * -----------------------------------------------------------------------------
+ */
+// DEFINE: Package(s).
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+
+// TASK: Default
+gulp.task('default', function () {
+    console.log('Works fine!');
+});
+
+// TASK: JADE
+gulp.task('jade', function () {
+    gulp.src('./view/*.jade')
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./'))
+});
