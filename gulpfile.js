f6c0a8f80c0f3f87cea9c92a5a621100a44f6204
Emre Can ÖZTAŞ <me@emrecanoztas.com>
2020-07-07 14:33:22 +0300

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,32 @@
+/**
+ * JADE-KIT for JADE development.
+ *
+ *
+ *
+ * @package     jade-kit
+ * @author      ÖZTAŞ, Emre Can <me@emrecanoztas.com>
+ * @copyright   2020
+ * @license     MIT
+ * @link        https://github.com/ecoztas/jade-kit
+ * @since       v1.0
+ */
+
 // DEFINE: Package(s).
+// =============================================================================
 var gulp = require('gulp');
 var jade = require('gulp-jade');
 
 // TASK: Default
+// =============================================================================
 gulp.task('default', function () {
     console.log('Works fine!');
 });
 
 // TASK: JADE
+// =============================================================================
 gulp.task('jade', function () {
     return (
-        gulp.src('view/*.jade')
+        gulp.src('./*.jade')
             .pipe(jade({
                 pretty: true
             }))
