62ff0b08c63b43d90375f79b21fecf332682cc8d
Emre Can ÖZTAŞ <me@emrecanoztas.com>
2020-11-12 13:33:57 +0300

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -13,23 +13,24 @@
 
 // DEFINE: Package(s).
 // =============================================================================
-var gulp = require('gulp');
-var jade = require('gulp-jade');
+var gulp = require("gulp");
+var jade = require("gulp-jade");
 
 // TASK: Default
 // =============================================================================
-gulp.task('default', function () {
-    console.log('Works fine!');
+gulp.task("default", function () {
+    console.log("Works fine!");
 });
 
 // TASK: JADE
 // =============================================================================
-gulp.task('jade', function () {
-    return (
-        gulp.src('./*.jade')
-            .pipe(jade({
-                pretty: true
-            }))
-            .pipe(gulp.dest('./'))
-    );
+gulp.task("jade", function () {
+    return gulp
+        .src("./*.jade")
+        .pipe(
+            jade({
+                pretty: true,
+            })
+        )
+        .pipe(gulp.dest("./"));
 });
